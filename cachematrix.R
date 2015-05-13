5f3b39da543f5aa1a459f55e7e36de88e489da04
Charles Wang <Dancerww@gmail.com>
2015-05-13 14:46:32 +0800

    implemented functions in PA2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function cache the inverse of a given matrix, which avoids
+## repetitive computational-expensive 'solve' functions.
 
-## Write a short comment describing this function
+## Create a matrix object containing a list of four functions,
+## namely to retrieve and assign the values of the matrix and its
+## inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse) inv <<- inverse
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+## Retrieve the inverse of a matrix from cache, or compute and
+## cache one if none exists. Note: the message("getting cached
+## data") for debugging purpose has been removed from the 'if'
+## clause as in the example.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) return(inv)
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinv(inv)
+  inv
 }
